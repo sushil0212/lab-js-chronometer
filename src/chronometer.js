c585229690b1393010cedf703109ca52103d2de7
“Sushil <sushilstar9@gmail.com>
2024-05-02 13:00:22 +0100

    solved

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,26 +1,44 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = null
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
-  }
+    const callback = () => {
+        console.log(this.currentTime++);
+        if (this.currentTime <= 3)
+            setTimeout(callback, 1000);
+        else
+            printTimeCallback();
+    };
+    setTimeout(callback, 1000);
+}
 
   getMinutes() {
-    // ... your code goes here
+    const totalMinutes = Math.floor(this.currentTime / 60);
+    return totalMinutes;
+    
   }
 
   getSeconds() {
+    const secondsPassed = this.currentTime % 60;
+
+    return secondsPassed; 
     // ... your code goes here
   }
 
   computeTwoDigitNumber(value) {
+    const stringValue = String(value);
+
+    // Pad the string with a leading '0' if it's a single-digit number
+    return stringValue.padStart(2, '0');
     // ... your code goes here
   }
 
-  stop() {
-    // ... your code goes here
+  stop(chronometer) {
+    
   }
 
   reset() {
